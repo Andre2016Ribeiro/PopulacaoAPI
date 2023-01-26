993bb43651e850e7c8aa21ee06f1788d91465790
vitor.andre.ribeiro <vitor.andre.ribeiro@gmail.com>
2023-01-26 19:40:07 +0000

    Adicionar arquivos de projeto.

---
+++ Data/PopulacaoAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClassPopulacao;
+
+namespace PopulacaoAPI.Data
+{
+    public class PopulacaoAPIContext : DbContext
+    {
+        public PopulacaoAPIContext (DbContextOptions<PopulacaoAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ClassPopulacao.Country> Country { get; set; } = default!;
+    }
+}
